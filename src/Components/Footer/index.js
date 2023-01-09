a2fb5f6f86e01b35e8d2ccd6bdaafb516cfefb80
pabloschivazappa <pablo.schivazappa@gmail.com>
2023-01-09 12:17:12 -0300

    corrections of the responsive design

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className={styles.copyright}>
                 Copyright © {new Date().getFullYear()}
             </div>
-            <div>
+            <div className={styles.socials}>
                 <a href='https://www.instagram.com/pablo.schivazappa/' target="_blank" rel="noreferrer"><i class="fa-brands fa-instagram"></i></a>
                 <a href='https://www.youtube.com/channel/UCR16IUxEZ4amDETpPicvVmA' target="_blank" rel="noreferrer"><i class="fa-brands fa-youtube"></i></a>
             </div>
--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -16,8 +16,10 @@ const Projects = () => {
                 <div className={styles.content}>
                     <h3>TrackGenix</h3>
                     <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor N. del T. persona que se dedica a la imprenta desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.</p>
+                    <div className={styles.porjectBtns}>
                     <a href="https://trackgenix-app-rosy.vercel.app/home" target="_blank" rel="noreferrer" className={styles.seeLiveBtn}>See Live</a>
                     <a href="https://github.com/pabloschivazappa/Trackgenix-app" target="_blank" rel="noreferrer" className={styles.sourceCodeBtn}>Source Code</a>
+                    </div>
                 </div>
             </div>
         </section>
