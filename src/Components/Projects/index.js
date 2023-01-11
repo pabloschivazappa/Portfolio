49c97dad9b9ef0514dc84415c06a6ea0ca60850b
pabloschivazappa <pablo.schivazappa@gmail.com>
2023-01-11 11:23:20 -0300

    add project paragraph

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -8,14 +8,14 @@ const Projects = () => {
         <section id="projects" className={styles.container}>
             <h3 className={styles.projectTitle} data-aos="zoom-out">Projects</h3>
             <div className={styles.project}>
-                <Tilt style={{margin: 0}} data-aos="fade-right">
+                <Tilt style={{margin: 0}}>
                     <div className={styles.card}>
                         <img src={Trackgenix} alt="TrackGenix-project" />
                     </div>
                 </Tilt>
                 <div className={styles.content} data-aos="fade-left">
                     <h3>TrackGenix</h3>
-                    <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor N. del T. persona que se dedica a la imprenta desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.</p>
+                    <p>Trackgenix is ​​a web application that helps organize companies' assignments. You can access through the login, and depending on your role you will have certain functionalities and permissions; for example: create or edit projects, add employees to projects, create tasks, track hours, and much more...</p>
                     <div className={styles.porjectBtns}>
                     <a href="https://trackgenix-app-rosy.vercel.app/home" target="_blank" rel="noreferrer" className={styles.seeLiveBtn}>See Live</a>
                     <a href="https://github.com/pabloschivazappa/Trackgenix-app" target="_blank" rel="noreferrer" className={styles.sourceCodeBtn}>Source Code</a>
